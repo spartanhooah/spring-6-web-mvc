fc97ed61b6ba865f4bd49bf489e50139aee66c4b
Ben Frey <spartanhooah@gmail.com>
2023-02-17 20:35:47 -0500

    add controller annotation

--- src/main/java/net/frey/spring6webmvc/controller/BeerController.java
+++ src/main/java/net/frey/spring6webmvc/controller/BeerController.java
@@ -3,9 +3,11 @@ package net.frey.spring6webmvc.controller;
 import lombok.RequiredArgsConstructor;
 import net.frey.spring6webmvc.model.Beer;
 import net.frey.spring6webmvc.service.BeerService;
+import org.springframework.stereotype.Controller;
 
 import java.util.UUID;
 
+@Controller
 @RequiredArgsConstructor
 public class BeerController {
     private final BeerService beerService;
