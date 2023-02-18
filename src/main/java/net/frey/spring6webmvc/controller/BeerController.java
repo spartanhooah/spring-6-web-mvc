73963f8f5129e06a92d3c0ca5aa50d84d81f2d11
Ben Frey <spartanhooah@gmail.com>
2023-02-18 09:36:52 -0500

    start making the project RESTful

--- src/main/java/net/frey/spring6webmvc/controller/BeerController.java
+++ src/main/java/net/frey/spring6webmvc/controller/BeerController.java
@@ -3,23 +3,27 @@ package net.frey.spring6webmvc.controller;
 import lombok.RequiredArgsConstructor;
 import net.frey.spring6webmvc.model.Beer;
 import net.frey.spring6webmvc.service.BeerService;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/api/v1/beer")
 @RequiredArgsConstructor
 public class BeerController {
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers() {
         return beerService.listBeers();
     }
 
-    public Beer getBeerById(UUID id) {
-        return beerService.getBeerById(id);
+    @RequestMapping(value = "/{beerId}", method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable UUID beerId) {
+        return beerService.getBeerById(beerId);
     }
 }
